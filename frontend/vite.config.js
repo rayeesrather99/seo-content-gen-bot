3d60dc8ee20d3fac853614e71aa11038c9e4d198
Rayees ahmad rather <rayeesrather35@gmail.com>
2025-01-08 17:43:40 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../dist' // Output directory for the build
+    outDir: './dist' // Output directory for the build
   }
 });

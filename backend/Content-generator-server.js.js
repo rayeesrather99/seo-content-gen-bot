0551a6460f5ff77bfa2a212428579bbe31530f62
Rayees ahmad rather <rayeesrather35@gmail.com>
2025-01-08 16:04:30 +0530

    update server file

--- backend/Content-generator-server.js.js
+++ backend/Content-generator-server.js.js
@@ -7,9 +7,10 @@ import generateSEO from './utils/seo-utils.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
+const frontendURL = process.env.FRONTEND_URL || 'http://localhost:5173';
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: frontendURL,
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
 }));
